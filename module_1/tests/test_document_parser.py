86a977b94ebb69af25748b1a14ee5a3c90fe17be
a_adamovich <a.adamovich@just-ai.com>
2024-09-22 12:58:05 +0300

    [Module 1] tuning

--- module_1/tests/test_document_parser.py
+++ module_1/tests/test_document_parser.py
@@ -6,7 +6,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from solution import parse_pdf_hh_resume, parse_docx_hh_resume, parse_doc_hh_resume
 
-# Базовые тесты выходных данных для pdf-parser
+# Технические тесты
 def test_pdf_output_format():
     dct = parse_pdf_hh_resume('module_1/data/resume.pdf')
     assert isinstance(dct, dict), 'Неправильный тип выходных данных'
@@ -41,7 +41,7 @@ def test_docx_output_keys():
     assert 'Position' in dct.keys(), 'Нет ключа <Position>'
     assert 'Experience' in dct.keys(), 'Нет ключа <Experience>'
 
-# Тесты бизнес-логики для pdf-parser
+# Тесты соблюдения бизнес-логики
 def test_pdf_parser_consistency():
     dct = parse_pdf_hh_resume('module_1/data/resume.pdf')
     assert dct['Email'] == 'alekander@pushkin.ru', 'Неверный Email'
--- module_1/tests/test_html_parser.py
+++ module_1/tests/test_html_parser.py
@@ -6,7 +6,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from solution.html_parser import parse_habr_career_html
 
-# Базовые тесты выходных данных
+# Технические тесты
 def test_output_format():
     df = parse_habr_career_html()
     assert isinstance(df, pd.DataFrame), 'Неправильный тип выходных данных'
@@ -22,7 +22,7 @@ def test_output_length():
     df = parse_habr_career_html(n_pages=1)
     assert len(df) == 25, 'Парсинг первой страницы дает 25 вакансий'
 
-# Тест на бизнес-логику
+# Тесты соблюдения бизнес-логики
 def test_vacancy_existency():
     df = parse_habr_career_html(n_pages=3, employment_type='part_time')
     assert df.loc[df['Vacancy']=='Стажер DevSecOps', 'Company Name'].values[0] == 'Солар'
